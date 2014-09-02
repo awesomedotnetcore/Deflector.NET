f15be4bac0acc9cd91ef5d185808c10c1b3b373c
Philip Laureano <philip.laureano@gmail.com>
2014-09-02 16:49:29 +1000

    Added multiple assembly rewriting support to the deflector task

--- Deflector/Deflector.MSBuild.Tasks/DeflectorTask.cs
+++ Deflector/Deflector.MSBuild.Tasks/DeflectorTask.cs
@@ -12,49 +12,49 @@ namespace Deflector.MSBuild.Tasks
 {
     public class DeflectorTask : Task
     {
-        /// <summary>
-        /// Gets or sets the value indicating the full path and filename of the target assembly.
-        /// </summary>
-        /// <value>The target assembly filename.</value>
         [Required]
-        public string TargetFile { get; set; }
-
-        /// <summary>
-        /// Gets or sets the value indicating the full path and filename of the output assembly.
-        /// </summary>
-        /// <value>The output assembly filename.</value>
-        /// <remarks>This field is optional; if blank, the default value will be the same value as the <see cref="TargetFile"/> property.</remarks>
-        public string OutputFile { get; set; }
+        public ITaskItem[] TargetFiles { get; set; }
 
         public override bool Execute()
         {
             bool result = false;
 
-            // The output file name will be the same as the target
-            // file by default 
-            var outputFile = OutputFile;
-            if (string.IsNullOrEmpty(outputFile))
-                outputFile = TargetFile;
+            foreach (var item in TargetFiles)
+            {
+                var targetFile = item.ItemSpec;
+                result = Rewrite(targetFile, result);
+                if (!result)
+                    return false;
+            }
+            
+
+            return result;
+        }
+
+        private bool Rewrite(string targetFile, bool result)
+        {
+            var outputFile = targetFile;
 
             try
             {
-                Log.LogMessage(MessageImportance.Normal, "{0}: Adding method call interception to assembly '{1}' (Output File: {2})", GetType().Name, TargetFile, OutputFile);
-                var assembly = AssemblyDefinition.ReadAssembly(TargetFile);
+                Log.LogMessage(MessageImportance.Normal,
+                    "{0}: Adding method call interception to assembly '{1}' (Output File: {2})", GetType().Name, targetFile,
+                    outputFile);
+                var assembly = AssemblyDefinition.ReadAssembly(targetFile);
 
                 var emitter = new MethodCallInterceptionEmitter();
                 emitter.Rewrite(assembly);
 
-                var parameters = new WriterParameters() { WriteSymbols = true };
+                var parameters = new WriterParameters() {WriteSymbols = true};
                 assembly.Write(outputFile, parameters);
 
                 result = true;
             }
             catch (Exception exception)
             {
-                Log.LogError("Unknown error while trying to modify assembly '{0}'", TargetFile);
+                Log.LogError("Unknown error while trying to modify assembly '{0}'", targetFile);
                 Log.LogErrorFromException(exception);
             }
-
             return result;
         }
     }
