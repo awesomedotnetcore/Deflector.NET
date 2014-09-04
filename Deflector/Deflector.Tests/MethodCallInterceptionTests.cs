70701754b50e52ea32fd50b7212b0433df1ab64f
Philip Laureano <philip.laureano@gmail.com>
2014-09-04 12:07:43 +1000

    Added the method call interception tests

---
+++ Deflector/Deflector.Tests/MethodCallInterceptionTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Mono.Cecil;
+using NUnit.Framework;
+using SampleLibrary;
+
+namespace Deflector.Tests
+{
+    [TestFixture]
+    public class MethodCallInterceptionTests : BaseAssemblyVerificationTestFixture
+    {
+        [Test]
+        public void Should_intercept_static_method()
+        {
+            var assemblyLocation = typeof(SampleClassWithInstanceMethod).Assembly.Location;
+            var assemblyDefinition = AssemblyDefinition.ReadAssembly(assemblyLocation);
+            var emitter = new MethodCallInterceptionEmitter();
+            emitter.Rewrite(assemblyDefinition);
+
+            var callCount = 0;
+            Action<string> incrementCallCount = text =>
+            {
+                callCount++;
+            };
+
+            Replace.Method(() => Console.WriteLine("")).With(incrementCallCount);
+
+            var assembly = assemblyDefinition.ToAssembly();
+            var targetType = assembly.GetTypes().First(t => t.Name == "SampleClassWithInstanceMethod");
+
+            var targetMethod = targetType.GetMethods().First(m=>m.IsStatic && m.Name == "DoSomething");
+            targetMethod.Invoke(null, new object[0]);
+
+            Assert.AreEqual(1, callCount);
+        }
+    }
+}
